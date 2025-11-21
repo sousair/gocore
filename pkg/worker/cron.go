e20c2b5cf442e1358cbc542110bdd16be2b31a4b
sousair <sousair2001@gmail.com>
2025-11-21 13:39:59 -0300

    refactor(worker): cron worker

--- pkg/worker/cron.go
+++ pkg/worker/cron.go
@@ -6,42 +6,78 @@ import (
 	"time"
 )
 
-type CronJob struct {
-	Name     string
-	Fn       func(ctx context.Context) error
+const (
+	CRON_DEFAULT_TIMEOUT  = time.Minute
+	CRON_DEFAULT_INTERVAL = time.Second
+)
+
+type CronJob interface {
+	Name() string
+	Handle(context.Context) error
+}
+
+type cronOptions struct {
 	Interval time.Duration
 }
 
+type CronOption func(*cronOptions)
+
+func WithCronInterval(interval time.Duration) CronOption {
+	return func(co *cronOptions) {
+		co.Interval = interval
+	}
+}
+
+type cronJob struct {
+	Handle  func(context.Context) error
+	Options *cronOptions
+}
+
 type CronWorker struct {
-	jobs []*CronJob
+	jobs map[string]*cronJob
 }
 
 func NewCronWorker() *CronWorker {
-	return &CronWorker{}
+	return &CronWorker{
+		jobs: make(map[string]*cronJob),
+	}
 }
 
-func (cw *CronWorker) RegisterJob(job *CronJob) {
-	cw.jobs = append(cw.jobs, job)
+// Use WithCronInterval to set options
+func (cw *CronWorker) RegisterJob(job CronJob, opts ...CronOption) {
+	options := &cronOptions{
+		Interval: CRON_DEFAULT_INTERVAL,
+	}
+
+	for _, opt := range opts {
+		opt(options)
+	}
+
+	cw.jobs[job.Name()] = &cronJob{
+		Handle:  job.Handle,
+		Options: options,
+	}
 }
 
 func (cw CronWorker) Start(ctx context.Context) {
-	for _, job := range cw.jobs {
-		go func(job *CronJob) {
+	for name, job := range cw.jobs {
+		go func(name string, job *cronJob) {
 			for {
+				jobOptions := job.Options
 				select {
 				case <-ctx.Done():
-					fmt.Printf("[CronWorker] Stopping (%s) job \n", job.Name)
+					fmt.Printf("[CronWorker] Stopping (%s) job \n", name)
+				case <-time.After(jobOptions.Interval):
+					fmt.Printf("[CronWorker] Running (%s) job \n", name)
 
-				case <-time.After(job.Interval):
-					fmt.Printf("[CronWorker] Running (%s) job \n", job.Name)
-
-					if err := job.Fn(ctx); err != nil {
-						fmt.Printf("[CronWorker] Job (%s) returned error: %v \n", job.Name, err)
+					if err := job.Handle(ctx); err != nil {
+						fmt.Printf("[CronWorker] Job (%s) returned error: %v \n", name, err)
 					}
 
-					fmt.Printf("[CronWorker] Job (%s) finished \n", job.Name)
+					fmt.Printf("[CronWorker] Job (%s) finished \n", name)
 				}
+
 			}
-		}(job)
+		}(name, job)
 	}
 }
