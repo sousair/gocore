d4eedec25ad081a077b04313f7cf19ed6d43f136
sousair <sousair2001@gmail.com>
2025-11-22 00:09:15 -0300

    feat(database): paginated repository

--- pkg/database/repository/options.go
+++ pkg/database/repository/options.go
@@ -7,14 +7,35 @@ import (
 
 type Option func(*gorm.DB) *gorm.DB
 
-func WithRelation(association string) Option {
-	return func(d *gorm.DB) *gorm.DB {
-		return d.Preload(association)
+func WithDeleted() Option {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Unscoped()
+	}
+}
+
+func WithRelation(relations ...string) Option {
+	return func(db *gorm.DB) *gorm.DB {
+		for _, relation := range relations {
+			db = db.Preload(relation)
+		}
+
+		return db
 	}
 }
 
 func WithAllRelations() Option {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Preload(clause.Associations)
+	}
+}
+
+func WithRowLock(allowRead bool) Option {
+	return func(db *gorm.DB) *gorm.DB {
+		lock := clause.Locking{Strength: clause.LockingStrengthUpdate}
+		if allowRead {
+			lock = clause.Locking{Strength: clause.LockingStrengthShare}
+		}
+
+		return db.Clauses(lock)
 	}
 }
