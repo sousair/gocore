eea2d2f8556b16a5a3a44c7a4802031d9a1a9329
sousair <sousair2001@gmail.com>
2025-12-09 19:00:26 -0300

    feat(database): add a extendable metadata entity

---
+++ pkg/database/entity/metadata.go
@@ -0,0 +1,20 @@
+package entity
+
+import "encoding/json"
+
+type WithMetadata[T any] struct {
+	Metadata json.RawMessage `json:"metadata" gorm:"type:jsonb"`
+}
+
+func (e *WithMetadata[T]) GetMetadata() (*T, error) {
+	if e.Metadata == nil {
+		return new(T), nil
+	}
+
+	var metadata *T
+	if err := json.Unmarshal(e.Metadata, &metadata); err != nil {
+		return nil, err
+	}
+
+	return metadata, nil
+}
